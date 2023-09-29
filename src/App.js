120bc913b354079c2f7fd146db18af875223ad38
sakshi186 <sakshisisodiya186@gmail.com>
2023-09-28 18:54:23 -1200

    first assignment completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState , Component, useEffect }from 'react';
 import './App.css';
+import Chart from 'react-apexcharts'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App =()=>  {
+   
+  const[option,setOption]=useState({
+    chart: {
+      id: 'apexchart-example'
+    },
+    xaxis: {
+      categories: []
+    }
+  })
 
+  const[series,setSeries] = useState([{
+    name: 'series-1',
+    data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+  }])
+  
+  useEffect(()=>{
+    const xax=[]
+    const yax=[]
+    axios.get("https://api.llama.fi/summary/fees/lyra?dataType=dailyFees")
+    .then(response=>{
+        console.log("response", response)
+        response.data.totalDataChart.map(item =>{
+          console.log("item",item[0],item[1])
+          xax.push(item[0]);
+          yax.push(item[1])
+        })
+        console.log(xax,"xax")
+        console.log(yax,"yax")
+        setOption({
+          chart: {
+            id: 'apexchart-example'
+          },
+          xaxis: {
+            categories:xax
+          }
+        })
+        setSeries([{
+          name: 'series-1',
+          data: yax
+        }])
+    })
+    .catch(error=>{
+      console.log("error", error)
+    })
+  },[])
+  
+    return (
+      <Chart options={option} series={series} type="line" width={1000} height={500} />
+    )
+  }
 export default App;
